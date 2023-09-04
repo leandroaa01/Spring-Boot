8a95f3ffa689e32d10599468ff53290215dec166
Leandro Andrade <126919973+leandroaa01@users.noreply.github.com>
2023-09-04 19:35:14 +0000

    Criando o modelo para aplicação

---
+++ src/main/java/ifrn/pi/eventos/Models/Events.java
@@ -0,0 +1,35 @@
+package ifrn.pi.eventos.Models;
+
+public class Events {
+    private String name;
+    private String local;
+    private String date;
+    private String time;
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLocal() {
+        return local;
+    }
+    public void setLocal(String local) {
+        this.local = local;
+    }
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+    public String getTime() {
+        return time;
+    }
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+
+}
