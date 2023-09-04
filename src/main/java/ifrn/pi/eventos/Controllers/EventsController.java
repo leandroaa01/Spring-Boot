7bc3add953c13e290b20e594deed44c66c7b4f06
Leandro Andrade <126919973+leandroaa01@users.noreply.github.com>
2023-09-04 19:43:43 +0000

    criando o controller de Event

---
+++ src/main/java/ifrn/pi/eventos/Controllers/EventsController.java
@@ -0,0 +1,12 @@
+package ifrn.pi.eventos.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventsController {
+    @RequestMapping("/Events/form")
+    public String form(){
+        return "formEvent";
+    }
+}
