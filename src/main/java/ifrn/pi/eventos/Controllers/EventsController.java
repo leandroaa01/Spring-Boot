0749643e9b5bea17513010874f0561503402926c
Leandro Andrade <126919973+leandroaa01@users.noreply.github.com>
2023-09-05 17:10:42 +0000

    exercício que é Eara tentarem fazer sozinhos:

--- src/main/java/ifrn/pi/eventos/Controllers/EventsController.java
+++ src/main/java/ifrn/pi/eventos/Controllers/EventsController.java
@@ -2,11 +2,28 @@ package ifrn.pi.eventos.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class EventsController {
     @RequestMapping("/Events/form")
     public String form(){
         return "formEvent";
+    }
+
+    @RequestMapping("/Events/processarFormulario")
+    public String processarFormulario(
+            @RequestParam("Nome") String nome,
+            @RequestParam("local") String local,
+            @RequestParam("data") String data,
+            @RequestParam("time") String horario) {
+        
+        System.out.println("Nome: " + nome);
+        System.out.println("Local: " + local);
+        System.out.println("Data: " + data);
+        System.out.println("Horário: " + horario);
+
+        // Redirecione para uma página de confirmação
+        return "confirmacao";
     }
 }
