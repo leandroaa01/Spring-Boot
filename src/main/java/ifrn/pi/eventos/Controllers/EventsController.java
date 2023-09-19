414dcc409bde390c188c5bcaf2b11acb77d2e39c
Leandro Andrade <leandro.andrade@escolar.ifrn.edu.br>
2023-09-19 16:59:09 -0300

    aula06

--- src/main/java/ifrn/pi/eventos/Controllers/EventsController.java
+++ src/main/java/ifrn/pi/eventos/Controllers/EventsController.java
@@ -1,10 +1,12 @@
 package ifrn.pi.eventos.Controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -38,6 +40,21 @@ public class EventsController {
       ModelAndView mv = new ModelAndView("Events/lista");
       mv.addObject("Events", events);
      return mv;
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView detalhar(@PathVariable() Long id){
+        ModelAndView md = new ModelAndView();
+        Optional<Events> opt = er.findById(id);
+        if(opt.isEmpty()){
+            md.setViewName("redirect:/Events");
+            return md;
+        }
+     md.setViewName("Events/detalhes");
+     Events events = opt.get();
+
+     md.addObject("Events", events);
+     return md;
     }
     
 }
